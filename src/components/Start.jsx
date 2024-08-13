86522737e5a0aa053885ab4df0cefe921ba98ecc
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-08-13 10:15:58 +0200

    Made app responsive

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,7 +4,7 @@ const Start = (props) => {
               <h1 className="start-h1" >Quizzical</h1>
               <p className="start-p">Do you got what it takes???</p>
               <button onClick={props.start} className="start-button">Start quiz</button>
-          </section>
+      </section>
     )
   }
   
