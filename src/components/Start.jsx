d480880727a955e3b65b2cd4124044fc9343cfd5
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-08-13 10:24:23 +0200

    Made an intro animation

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,12 +1,19 @@
+import React, { useEffect } from 'react';
+
 const Start = (props) => {
+    useEffect(() => {
+        const startSection = document.querySelector('.start');
+        startSection.classList.add('fade-in');
+    }, []);
+
     return (
-      <section className="start">
-              <h1 className="start-h1" >Quizzical</h1>
-              <p className="start-p">Do you got what it takes???</p>
-              <button onClick={props.start} className="start-button">Start quiz</button>
-      </section>
-    )
-  }
-  
-  export default Start
+        <section className="start">
+            <h1 className="start-h1">Quizzical</h1>
+            <p className="start-p">Do you got what it takes???</p>
+            <button onClick={props.start} className="start-button">Start quiz</button>
+        </section>
+    );
+}
+
+export default Start;
   
